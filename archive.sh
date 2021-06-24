b80a3d914a1c745721cdc73d939011ef4352e945
jay.choi <jay.choi@kakaocorp.com>
2021-06-24 23:46:56 +0900

    update archive.sh for clean build directory

--- archive.sh
+++ archive.sh
@@ -1,3 +1,6 @@
+echo "Cleaning..."
+rm -rf ./build
+
 echo "Archiving..."
 xcodebuild archive -scheme SwiftDemangle-Package -archivePath "./build/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive -scheme SwiftDemangle-Package  -archivePath "./build/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
@@ -9,3 +12,6 @@ xcodebuild -create-xcframework \
 -framework "./build/ios_sim.xcarchive/Products/Library/Frameworks/SwiftDemangle.framework" \
 -framework "./build/mac.xcarchive/Products/Library/Frameworks/SwiftDemangle.framework" \
 -output "./Binary/SwiftDemangle.xcframework"
+
+echo "Cleaning..."
+rm -rf ./build
