6b226ad2a82917b8cbfce230c132afc528c3c4d1
jay.choi <jay.choi@kakaocorp.com>
2021-12-09 13:15:52 +0900

    update archive.sh for xcframework platform destination

--- archive.sh
+++ archive.sh
@@ -3,9 +3,9 @@ rm -rf ./build
 rm -rf ./Binary
 
 echo "Archiving..."
-xcodebuild archive -scheme SwiftDemangle-Package -archivePath "./build/ios.xcarchive" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-xcodebuild archive -scheme SwiftDemangle-Package  -archivePath "./build/ios_sim.xcarchive" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-xcodebuild archive -scheme SwiftDemangle-Package  -archivePath "./build/mac.xcarchive" -sdk macosx SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme SwiftDemangle-Package -archivePath "./build/ios.xcarchive" -sdk iphoneos -destination generic/platform=iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme SwiftDemangle-Package  -archivePath "./build/ios_sim.xcarchive" -sdk iphonesimulator -destination 'generic/platform=iOS Simulator' SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme SwiftDemangle-Package  -archivePath "./build/mac.xcarchive" -sdk macosx -destination generic/platform=macOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 echo "Create XCFramework"
 xcodebuild -create-xcframework \
