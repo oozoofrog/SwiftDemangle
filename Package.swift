90062fb1e96aadb391d85204d193343191fb132b
jay.choi <jay.choi@kakaocorp.com>
2021-12-17 10:10:37 +0900

    remove all_load

--- Package.swift
+++ Package.swift
@@ -26,8 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftDemangle",
-            dependencies: [],
-            linkerSettings: [.unsafeFlags(["all_load"])]),
+            dependencies: []),
         .testTarget(
             name: "SwiftDemangleTests",
             dependencies: ["SwiftDemangle"],
