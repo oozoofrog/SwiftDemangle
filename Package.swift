b7708a49d127fb9bbac1c297b018e8c4b1aa5aa5
oozoofrog <oozoofrog@gmail.com>
2024-11-26 22:27:21 +0900

    remove version from Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .macOS(.v14),
         .iOS(.v16)
     ],
-    version: "6.0.3",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
